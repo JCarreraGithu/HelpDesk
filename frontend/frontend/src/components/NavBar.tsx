abe005c94fb8d84283b37cf7e1bff4a3b2cfde72
JCarreraGithu <jimmycarrera202@gmail.com>
2025-09-25 00:31:26 -0600

    se cambio posicicon de boton cerrar sesion

--- frontend/frontend/src/components/NavBar.tsx
+++ frontend/frontend/src/components/NavBar.tsx
@@ -1,22 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import sesionIcon from "../assets/sesion.png";
 
 export default function Navbar() {
   const navigate = useNavigate();
 
   return (
     <>
-      <header>
-        <img src={logo} alt="Logo Help Desk" className="logo" />
-        <h1>Help Desk</h1>
+      {/* HEADER */}
+      <header className="navbar-header">
+        <div className="navbar-left">
+          <img src={logo} alt="Logo Help Desk" className="logo" />
+          <h1>Help Desk</h1>
+        </div>
+
+        {/* Ícono de cerrar sesión */}
+        <button onClick={() => navigate("/")} className="logout-button">
+          <img src={sesionIcon} alt="Cerrar sesión" className="logout-icon" />
+        </button>
       </header>
 
+      {/* NAV */}
       <nav>
         <Link to="/dashboard/ver-casos" className="active">Casos</Link>
         <Link to="/dashboard/crear-caso">Nuevo Reporte</Link>
-        <button onClick={() => navigate("/")} className="nav-button">
-          Cerrar Sesión
-        </button>
       </nav>
     </>
   );
