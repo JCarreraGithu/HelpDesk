dfd032aa2fa82aad6cf154faaa27ffe591406f08
Marlon599 <lopezambrocio10@gmail.com>
2025-10-22 00:58:27 -0600

    Cambios

--- frontend/frontend/src/pages/Dashboard.tsx
+++ frontend/frontend/src/pages/Dashboard.tsx
@@ -8,15 +8,12 @@ import PerfilUsuario from "./PerfilUsuario";
 import { useState, useEffect } from "react";
 import "./Dashboard.css";
 import Empleados from "./Empleados";
-<<<<<<< HEAD
 import Estadistics from "./Estadistics";
 import Incidencias from "./Incidencias";
 import Repuestos from "./Repuestos";
 import Departamentos from "./Departamentos";
 import Puestos from "./Puestos";
 
-=======
->>>>>>> jimmy
 import FormularioEncuestaPremium from "./FormularioEncuestaPremium";
 import Reportes from "./Reportes";
 import ReporteCategoria from "./ReporteCategoria.tsx";
@@ -72,18 +69,15 @@ export default function Dashboard() {
   <Route path="usuarios" element={<DashboardUsuarios />} />
   <Route path="perfil" element={<PerfilUsuario />} />
   <Route path="empleados" element={<Empleados />} />  
-<<<<<<< HEAD
    <Route path="estadisticas" element={<Estadistics />} />  
    <Route path="incidencias" element={<Incidencias />} /> 
    <Route path="repuestos" element={<Repuestos />} /> 
       <Route path="departamentos" element={<Departamentos />} />      
-      <Route path="puestos" element={<Puestos />} /> 
-=======
->>>>>>> jimmy
-<Route path="calificar-servicio/:casoId" element={<FormularioEncuestaPremium usuarioId={1} />} />
-<Route path="/reportes" element={<Reportes />} />
-<Route path="reportes/:categoria" element={<ReporteCategoria />} />
-
+      <Route path="puestos" element={<Puestos />} />
+  {/*Rutas comentadas para futuras funcionalidades */}
+  <Route path="formulario-encuesta-premium" element={<FormularioEncuestaPremium />} /> 
+  <Route path="reportes" element={<Reportes />} /> 
+  <Route path="reporte-categoria" element={<ReporteCategoria />} />
 
 
 
--- frontend/frontend/src/pages/ReporteCategoria.tsx
+++ frontend/frontend/src/pages/ReporteCategoria.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import ReporteTecnicos from "./reportes/ReporteTecnicos.tsx";
 import ReporteCasos from "./reportes/ReporteCasos.tsx";
 import ReporteSatisfaccion from "./reportes/ReporteSatisfaccion.tsx";
-import ReporteRepuestos from "./reportes/reporteRepuestos.tsx";
+//import ReporteRepuestos from "./reportes/reporteRepuestos.tsx";
 import ReporteDepartamentos from "./reportes/ReporteDepartamentos.tsx";
 import ReporteEmpleados from "./reportes/ReporteEmpleados.tsx";
 
@@ -17,6 +17,6 @@ export default function ReporteCategoria() {
   if (categoria === "empleados") return <ReporteEmpleados/>;
   if (categoria === "departamentos") return <ReporteDepartamentos />;
   if (categoria === "satisfaccion")return <ReporteSatisfaccion />;
-  if (categoria === "repuestos") return <ReporteRepuestos />;
+//if (categoria === "repuestos") return <ReporteRepuestos />;
   return <div className="container py-4">⚠️ Categoría no encontrada.</div>;
 }
